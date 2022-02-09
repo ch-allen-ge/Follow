2b3de332cf43366734989c19007d4c25a3985b89
Allen Tan <tanalle@MFCGD.COM>
2022-02-09 17:22:50 -0500

    Created a skeleton for the front page

--- webpack.config.js
+++ webpack.config.js
@@ -14,15 +14,22 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: ['@babel/preset-env', ['@babel/preset-react', {"runtime": "automatic"}]]
           }
         }
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
+      template: path.join(__dirname, "public", "index.html"),
     }),
   ],
 }
